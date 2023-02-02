b8bc3a30170520dd1eed978d474d181ec644d78d
berti <berti@CLASSES.LOCAL>
2023-02-02 09:55:14 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="build/style.css">
+    <title>School Website</title>
+</head>
+<body>
+    <div class="slide" id="home">
+        <header>
+            <div id="info">
+                <div class="wrapper">
+                    <div id="pratique">
+                        <span id="tel">Tel: 02 / 395.60.23 </span>
+                        <span id="email">E-mail: info@epse.be</span>
+                    </div>
+                    <div id="social">Social media icons</div>
+                </div>
+            </div>
+            <div id="header">
+                <div class="wrapper">
+                    <div id="logo">Site Name</div>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Schools</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+
+    </div>
+</body>
+</html>
